a8ce5f4c011b02ac8059c72103e2301a7a395633
AhmadK <ahmad31100@live.fr>
2024-12-10 21:09:05 +0100

    fixed path to feature folder

--- TP2/cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ TP2/cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -2,8 +2,12 @@ package dojo;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/main/ressources/dojo", // Dossier contenant le fichier .feature
+    glue = "dojo")
 public class RunCucumberTest {
 }
